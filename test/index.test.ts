19e9a4b330e646b0232872d8ee6eed71938b0305
fiahfy <d.fiahfy@gmail.com>
2019-11-12 00:53:01 +0900

    Update index.test.ts

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,12 +3,12 @@ import Icns from '../src'
 
 describe('icns', () => {
   describe('constructor', () => {
-    test('should work', async () => {
+    test('should work', () => {
       const icns = new Icns()
       expect(icns.images.length).toBe(0)
     })
 
-    test('should work with buffer', async () => {
+    test('should work with buffer', () => {
       const buf = fs.readFileSync('./test/sample.icns')
       const icns = new Icns(buf)
       expect(icns.images.length).toBe(10)
@@ -25,42 +25,42 @@ describe('icns', () => {
   })
 
   describe('appendImage', () => {
-    test('should work for PNG', async () => {
+    test('should work for PNG', () => {
       const buf = fs.readFileSync('./test/1024x1024.png')
       const icns = new Icns()
       expect(icns.images.length).toBe(0)
-      await icns.appendImage(buf, 'ic10')
+      icns.appendImage(buf, 'ic10')
       expect(icns.images.length).toBe(1)
-      await icns.appendImage(buf, 'ic10')
+      icns.appendImage(buf, 'ic10')
       expect(icns.images.length).toBe(2)
     })
 
-    test('should work for ARGB', async () => {
+    test('should work for ARGB', () => {
       const buf = fs.readFileSync('./test/16x16.png')
       const icns = new Icns()
       expect(icns.images.length).toBe(0)
-      await icns.appendImage(buf, 'ic04')
+      icns.appendImage(buf, 'ic04')
       expect(icns.images.length).toBe(1)
-      await icns.appendImage(buf, 'ic04')
+      icns.appendImage(buf, 'ic04')
       expect(icns.images.length).toBe(2)
     })
 
     test('should throw error if buffer is not PNG format', () => {
       const buf = fs.readFileSync('./test/1024x1024.jpg')
       const icns = new Icns()
-      expect(icns.appendImage(buf, 'ic10')).rejects.toThrowError(TypeError)
+      expect(() => icns.appendImage(buf, 'ic10')).toThrowError(TypeError)
     })
 
     test('should throw error if buffer is not square', () => {
       const buf = fs.readFileSync('./test/256x128.png')
       const icns = new Icns()
-      expect(icns.appendImage(buf, 'ic10')).rejects.toThrowError(TypeError)
+      expect(() => icns.appendImage(buf, 'ic10')).toThrowError(TypeError)
     })
 
     test('should throw error if buffer is not supported size', () => {
       const buf = fs.readFileSync('./test/100x100.png')
       const icns = new Icns()
-      expect(icns.appendImage(buf, 'ic10')).rejects.toThrowError(TypeError)
+      expect(() => icns.appendImage(buf, 'ic10')).toThrowError(TypeError)
     })
   })
 })
